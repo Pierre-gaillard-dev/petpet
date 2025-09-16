27d39bc83b689f811ba5d0e7774279eb77a7f157
Pierre <pierre.gaillard.dev@gmail.com>
2025-09-16 14:48:08 +0200

    feat(init): create react routes and pages

---
+++ front/src/pages/Feed.tsx
@@ -0,0 +1,7 @@
+import type { FC } from "react"
+
+const Feed: FC = () => {
+  return <div className="text-3xl font-bold underline">Feed</div>
+}
+
+export default Feed
---
+++ front/src/pages/login.tsx
@@ -0,0 +1,7 @@
+import type { FC } from "react"
+
+const Login: FC = () => {
+  return <div className="text-3xl font-bold underline">Login</div>
+}
+
+export default Login
